c0c85542966162de3a082c70355917a10cbeca82
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-06-19 22:09:46 +0900

    構成変更

---
+++ FesTracking2/Application/Service/AuthService.swift
@@ -0,0 +1,13 @@
+//
+//  AuthService.swift
+//  FesTracking2
+//
+//  Created by 松下和也 on 2025/06/19.
+//
+
+struct AuthService: Sendable {
+    
+    let userRole: UserRole = .guest
+    let accessToken: String?
+    
+}
