9c56e77ceabd9ec675ce0d7995d421a5daef65c1
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-06-26 11:33:38 +0900

    追加

--- FesTracking2/Domain/Entities/Region/Region.swift
+++ FesTracking2/Domain/Entities/Region/Region.swift
@@ -15,11 +15,12 @@ struct Region: Codable, Equatable, Identifiable, Hashable {
     var city: String
     var base: Coordinate
     var spans: [Span] = []
+    var milestones: [Information] = []
     var imagePath:String?
 }
 
 
 extension Region {
-    static let sample = Self(id: "掛川祭_年番本部", name: "掛川祭", subname: "年番本部", description: "省略", prefecture: "静岡県", city: "掛川市", base: Coordinate.sample, spans: [Span.sample])
+    static let sample = Self(id: "掛川祭_年番本部", name: "掛川祭", subname: "年番本部", description: "省略", prefecture: "静岡県", city: "掛川市", base: Coordinate.sample, spans: [Span.sample], milestones:[])
 }
 
