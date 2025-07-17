46a166d19f0407417462dd8dce43f57b43f13d6e
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-07-17 15:47:13 +0900

    既存Viewの変更

--- FesTracking2/Presentation/Style/ButtonStyle.swift
+++ FesTracking2/Presentation/Style/ButtonStyle.swift
@@ -7,31 +7,39 @@
 
 import SwiftUI
 
-struct SecondaryButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .fontWeight(.semibold)
-            .foregroundColor(.blue)
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color.blue, lineWidth: 1.5)
-            )
-            .opacity(configuration.isPressed ? 0.6 : 1.0)
-    }
-}
 struct PrimaryButtonStyle: ButtonStyle {
+    var backgroundColor: Color = .blue
+    var foregroundColor: Color = .white
+
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .fontWeight(.semibold)
             .frame(maxWidth: .infinity)
             .padding()
-            .background(.blue)
-            .foregroundColor(.white)
+            .background(backgroundColor)
+            .foregroundColor(foregroundColor)
             .cornerRadius(8)
             .opacity(configuration.isPressed ? 0.6 : 1.0)
     }
 }
+
+struct SecondaryButtonStyle: ButtonStyle {
+    var borderColor: Color = .blue
+    var foregroundColor: Color = .blue
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .fontWeight(.semibold)
+            .foregroundColor(foregroundColor)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(borderColor, lineWidth: 1.5)
+            )
+            .opacity(configuration.isPressed ? 0.6 : 1.0)
+    }
+}
+
 
     
