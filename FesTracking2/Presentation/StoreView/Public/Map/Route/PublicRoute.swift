633603b6f7f796907000b85aad95e8ff10a10b14
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-08-05 16:19:44 +0900

    Map.Route改修

---
+++ FesTracking2/Presentation/StoreView/Public/Map/Route/PublicRoute.swift
@@ -0,0 +1,133 @@
+//
+//  RouteInfo.swift
+//  FesTracking2
+//
+//  Created by 松下和也 on 2025/05/04.
+//
+
+import Foundation
+import ComposableArchitecture
+import MapKit
+
+@Reducer
+struct PublicRoute {
+    
+    @CasePathable
+    enum Detail: Equatable{
+        case point(Point)
+        case location(LocationInfo)
+    }
+    
+    @ObservableState
+    struct State: Equatable {
+        let id: String
+        let items: [RouteSummary]?
+        var selectedItem: RouteSummary?
+        var route: RouteInfo?
+        var location: LocationInfo?
+        var isMenuExpanded: Bool = false
+        @Shared var mapRegion: MKCoordinateRegion?
+        var detail: Detail?
+        
+        var points: [Point]? {
+            if let route {
+                PointFilter.pub.apply(to: route)
+            } else {
+                nil
+            }
+        }
+        
+        var segments: [Segment]? {
+            route?.segments
+        }
+        
+        var others: [RouteSummary]? {
+            items?.filter {
+                if let selectedItem {
+                    $0.id != selectedItem.id
+                } else {
+                    false
+                }
+            }
+        }
+        
+        init(id: String, routes: [RouteSummary]? = nil, selectedRoute: RouteInfo? = nil, location: LocationInfo? = nil, mapRegion: Shared<MKCoordinateRegion?>){
+            self.items = routes
+            if let selectedRoute {
+                self.selectedItem = RouteSummary(from: selectedRoute)
+            }
+            self.route = selectedRoute
+            self.location = location
+            self._mapRegion = mapRegion
+        }
+    }
+    
+    @CasePathable
+    enum Action: Equatable, BindableAction {
+        case binding(BindingAction<State>)
+        case menuTapped
+        case itemSelected(RouteSummary)
+        case pointTapped(Point)
+        case reloadTapped
+        case locationTapped
+        case routeReceived(Result<RouteInfo, ApiError>)
+        case locationReceived(Result<LocationInfo, ApiError>)
+    }
+    
+    @Dependency(\.apiRepository) var apiRepository
+    @Dependency(\.authService) var authService
+    
+    var body: some ReducerOf<PublicRoute> {
+        BindingReducer()
+        Reduce{ state, action in
+            switch action {
+            case .binding(_):
+                return .none
+            case .menuTapped:
+                state.isMenuExpanded = true
+                return .none
+            case .itemSelected(let value):
+                state.isMenuExpanded = false
+                state.selectedItem = value
+                return .run { send in
+                    let accessToken = await authService.getAccessToken()
+                    let result = await apiRepository.getRoute(value.id, accessToken)
+                }
+            case .pointTapped(let value):
+                state.detail = .point(value)
+                return .none
+            case .locationTapped:
+                guard let location = state.location else { return .none }
+                state.detail = .location(location)
+                return .none
+            case .reloadTapped:
+                return .run {[id = state.id] send in
+                    let accessToken = await authService.getAccessToken()
+                    let result = await apiRepository.getLocation(id, accessToken)
+                }
+            case .routeReceived(.success(let value)):
+                state.route = value
+                state.$mapRegion.withLock { $0 = makeRegion(value.points.map{ $0.coordinate })}
+                return .none
+            case .routeReceived(.failure(let error)):
+                return .none
+            case .locationReceived(.success(let value)):
+                state.location = value
+                return .none
+            case .locationReceived(.failure(let error)):
+                return .none
+            }
+        }
+    }
+}
+
+extension PublicRoute.Detail: Identifiable {
+    var id: String {
+        switch self{
+        case .point(let item):
+            return item.id
+        case .location(let item):
+            return item.id
+        }
+    }
+}
---
+++ FesTracking2/Presentation/StoreView/Public/Map/Route/PublicRouteMap.swift
@@ -0,0 +1,125 @@
+//
+//  PublicRouteMap.swift
+//  FesTracking2
+//
+//  Created by 松下和也 on 2025/08/05.
+//
+
+import UIKit
+import MapKit
+import SwiftUI
+
+struct PublicRouteMap: UIViewRepresentable {
+    var points: [Point]?
+    var segments: [Segment]?
+    var location: LocationInfo?
+    var pointTapped: (Point)->Void
+    var locationTapped: ()->Void
+    @Binding var region: MKCoordinateRegion?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        if let region = region {
+            mapView.setRegion(region, animated: false)
+        }
+        return mapView
+    }
+
+    func updateUIView(_ mapView: MKMapView, context: Context) {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.removeOverlays(mapView.overlays)
+        
+        // アノテーション追加
+        if let points {
+            for point in points {
+                let annotation = PointAnnotation(point, type: .simple )
+                annotation.coordinate = point.coordinate.toCL()
+                mapView.addAnnotation(annotation)
+            }
+        }
+        
+        // ポリライン追加
+        if let segments{
+            for segment in segments {
+                let polyline = SegmentPolyline(coordinates: segment.coordinates.map({$0.toCL()}), count: segment.coordinates.count)
+                polyline.segment = segment
+                mapView.addOverlay(polyline)
+            }
+        }
+        
+        //ロケーション追加
+        if let location {
+            let annotation = LocationAnnotation(location: location)
+            annotation.coordinate = location.coordinate.toCL()
+            mapView.addAnnotation(annotation)
+        }
+        
+        if let region = region,
+            region.center.latitude != mapView.region.center.latitude
+            || region.center.longitude != mapView.region.center.longitude {
+            mapView.setRegion(region, animated: true)
+        }
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: PublicRouteMap
+        
+        init(_ parent: PublicRouteMap) {
+            self.parent = parent
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            if annotation is MKUserLocation {
+                return nil // 現在地はデフォルトのまま
+            }
+            let identifier = "AnnotationView"
+
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            if annotationView == nil {
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            } else {
+                annotationView?.annotation = annotation
+            }
+            annotationView?.displayPriority = .required
+            if #available(iOS 11.0, *) {
+                (annotationView as? MKMarkerAnnotationView)?.clusteringIdentifier = nil
+            }
+            if let markerView = annotationView as? MKMarkerAnnotationView {
+                markerView.markerTintColor = .red
+                markerView.canShowCallout = true
+            }
+            return annotationView
+        }
+        
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            if let annotation = view.annotation as? PointAnnotation {
+                parent.pointTapped(annotation.point)
+            } else if let  annotation = view.annotation as? LocationAnnotation {
+                parent.locationTapped()
+            }
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let polyline = overlay as? SegmentPolyline {
+                let renderer = MKPolylineRenderer(overlay: polyline)
+                renderer.strokeColor = .blue
+                renderer.lineWidth = 4
+                renderer.alpha = 0.8
+                return renderer
+            }
+            return MKOverlayRenderer()
+        }
+        
+        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+            parent.region = mapView.region
+        }
+    }
+}
+
+
+
