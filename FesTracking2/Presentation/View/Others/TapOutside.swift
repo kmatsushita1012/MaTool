88ccb69b0abaf3045ed0e4a9d237a6d2d5162b7f
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-08-07 00:16:28 +0900

    アウトサイドタップ

---
+++ FesTracking2/Presentation/View/Others/TapOutside.swift
@@ -0,0 +1,45 @@
+//
+//  TapOutside.swift
+//  FesTracking2
+//
+//  Created by 松下和也 on 2025/08/06.
+//
+
+import SwiftUI
+
+struct TapOutsideModifier: ViewModifier {
+    @Binding var isShown: Bool
+
+    @State private var isVisible = false
+
+    func body(content: Content) -> some View {
+        ZStack {
+            if isVisible {
+                Color.clear
+                    .contentShape(Rectangle())
+                    .ignoresSafeArea()
+                    .transition(.opacity)
+                    .onTapGesture {
+                        withAnimation{
+                            isShown = false
+                        }
+                    }
+            }
+            content
+        }
+        .onChange(of: isShown) {
+            withAnimation(.easeInOut(duration: 0.1)) {
+                isVisible = isShown
+            }
+        }
+        .onAppear {
+            isVisible = isShown
+        }
+    }
+}
+
+extension View {
+    func tapOutside(isShown: Binding<Bool>) -> some View {
+        self.modifier(TapOutsideModifier(isShown: isShown))
+    }
+}
