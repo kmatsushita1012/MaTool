4eb3b69da6a9db4a818cc71148ccc3949ef4b217
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-08-07 15:44:53 +0900

    FloatAnnotation実装

--- FesTracking2/Presentation/View/Map/MapFragments.swift
+++ FesTracking2/Presentation/View/Map/MapFragments.swift
@@ -5,7 +5,9 @@
 //  Created by 松下和也 on 2025/05/09.
 //
 
+import UIKit
 import MapKit
+import SwiftUI
 
 class PointAnnotation: MKPointAnnotation {
     enum TitleType {
@@ -31,16 +33,101 @@ class PointAnnotation: MKPointAnnotation {
     }
 }
 
+final class PointAnnotationView: MKMarkerAnnotationView {
+    override var annotation: MKAnnotation? {
+        willSet {
+            guard let _ = newValue as? PointAnnotation else { return }
+
+            // 細かい設定はここに書ける
+            displayPriority = .required
+
+            if #available(iOS 11.0, *) {
+                clusteringIdentifier = nil
+            }
+
+            markerTintColor = .red
+        }
+    }
+}
+
+
+extension PointAnnotationView {
+    static let identifier = "PointAnnotationView"
+
+    static func view(for mapView: MKMapView, annotation: PointAnnotation) -> MKAnnotationView {
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? PointAnnotationView
+
+        if annotationView == nil {
+            annotationView = PointAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        } else {
+            annotationView?.annotation = annotation
+        }
+
+        return annotationView!
+    }
+}
+
+
 class SegmentPolyline: MKPolyline {
     var segment: Segment? = nil
 }
 
-class LocationAnnotation: MKPointAnnotation {
+final class FloatAnnotation: MKPointAnnotation {
     let location: LocationInfo
     
     init(location: LocationInfo) {
         self.location = location
         super.init()
-        self.title = location.districtId
+        self.title = location.districtName
+    }
+}
+
+final class FloatAnnotationView: MKMarkerAnnotationView {
+    static let identifier = "FloatAnnotationView"
+    override var annotation: MKAnnotation? {
+        willSet {
+            canShowCallout = false
+            guard let baseImage = UIImage(systemName: "circle.circle.fill") else { return }
+
+            // サイズ拡大
+            let scale: CGFloat = 1.5
+            let newSize = CGSize(width: baseImage.size.width * scale,
+                                 height: baseImage.size.height * scale)
+
+            let imageView = UIImageView(image: baseImage)
+            imageView.bounds = CGRect(origin: .zero, size: newSize)
+            imageView.backgroundColor = .clear
+            imageView.tintColor = .red
+
+            // レンダリング
+            let renderer = UIGraphicsImageRenderer(size: newSize)
+            let renderedImage = renderer.image { _ in
+                imageView.drawHierarchy(in: CGRect(origin: .zero, size: newSize), afterScreenUpdates: true)
+            }
+
+            image = renderedImage
+            
+            displayPriority = .required
+            
+            markerTintColor = .clear
+            glyphTintColor = .clear
+            glyphImage = nil
+            
+        }
+    }
+}
+
+
+extension FloatAnnotationView {
+    static func view(for mapView: MKMapView, annotation: FloatAnnotation) -> MKAnnotationView {
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? FloatAnnotationView
+
+        if annotationView == nil {
+            annotationView = FloatAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        } else {
+            annotationView?.annotation = annotation
+        }
+
+        return annotationView!
     }
 }
