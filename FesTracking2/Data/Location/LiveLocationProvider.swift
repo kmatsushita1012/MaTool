2f1fca3e76b424c2f9f973ee0922183992965ad9
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-09-10 00:50:04 +0900

    Providerの修正

--- FesTracking2/Data/Location/LiveLocationProvider.swift
+++ FesTracking2/Data/Location/LiveLocationProvider.swift
@@ -10,44 +10,58 @@ import Combine
 import ComposableArchitecture
 
 actor LocationProvider: NSObject, LocationProviderProtocol {
-    private let manager: CLLocationManager
-    private var delegateProxy: DelegateProxy?
+    private(set) var manager: CLLocationManager? = nil
     private(set) var isTracking = false
     private(set) var value: AsyncValue<CLLocation> = .loading
 
     override init() {
-        self.manager = CLLocationManager()
         super.init()
-        self.delegateProxy = DelegateProxy(owner: self)
-        self.manager.delegate = delegateProxy
-        self.manager.allowsBackgroundLocationUpdates = true
+        
+        Task{
+            await setupLocationManagerIfNeeded()
+        }
+    }
+    
+    private func setupLocationManagerIfNeeded() async {
+        let manager = await MainActor.run {
+            let manager = CLLocationManager()
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.allowsBackgroundLocationUpdates = true
+            return manager
+        }
+        if self.manager == nil {
+            self.manager = manager
+        }
+        
     }
 
     // 権限リクエスト
-    func requestPermission() {
-        manager.requestWhenInUseAuthorization()
-        manager.requestAlwaysAuthorization()
+    func requestPermission() async {
+        await setupLocationManagerIfNeeded()
+        manager?.requestWhenInUseAuthorization()
+        manager?.requestAlwaysAuthorization()
     }
 
     // トラッキング開始
     func startTracking() {
-        manager.startUpdatingLocation()
+        manager?.startUpdatingLocation()
         isTracking = true
         
-        if let cached = manager.location {
+        if let cached = manager?.location {
             value = .success(cached)
         }
     }
 
     // トラッキング停止
     func stopTracking() {
-        manager.stopUpdatingLocation()
+        manager?.stopUpdatingLocation()
         isTracking = false
     }
 
     // 最新の状態を返す
     func getLocation() -> AsyncValue<CLLocation> {
-        if manager.authorizationStatus == .denied {
+        if manager?.authorizationStatus == .denied {
             return .failure(LocationError.authorizationDenied)
         }
         if !CLLocationManager.locationServicesEnabled() {
@@ -58,27 +72,21 @@ actor LocationProvider: NSObject, LocationProviderProtocol {
     }
 
     func isPermissionAllowed() -> Bool {
-        return manager.authorizationStatus != .denied
-    }
-    
-    private final class DelegateProxy: NSObject, CLLocationManagerDelegate {
-        weak var owner: LocationProvider?
-
-        init(owner: LocationProvider) {
-            self.owner = owner
-        }
-
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            guard let location = locations.last else { return }
-            Task { await owner?.updateValue(.success(location)) }
-        }
-
-        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-            Task { await owner?.updateValue(.failure(error)) }
-        }
+        return manager?.authorizationStatus != .denied
     }
 
     private func updateValue(_ newValue: AsyncValue<CLLocation>) {
         self.value = newValue
+    }
+}
+
+extension LocationProvider: CLLocationManagerDelegate {
+    nonisolated func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        Task{ await  updateValue(.success(location)) }
+    }
+
+    nonisolated func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        Task{ await updateValue(.failure(error)) }
     }
 }
