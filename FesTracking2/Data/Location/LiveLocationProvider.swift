c6bd2421b3e7aea7459e512e370b100eea49ef6e
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-09-18 07:16:55 +0900

    [Fix]Locationバックグラウンドモードの調整

--- FesTracking2/Data/Location/LiveLocationProvider.swift
+++ FesTracking2/Data/Location/LiveLocationProvider.swift
@@ -27,7 +27,7 @@ actor LocationProvider: NSObject, LocationProviderProtocol {
             let manager = CLLocationManager()
             manager.delegate = self
             manager.desiredAccuracy = kCLLocationAccuracyBest
-            manager.allowsBackgroundLocationUpdates = true
+            manager.pausesLocationUpdatesAutomatically = true
             return manager
         }
         if self.manager == nil {
@@ -44,7 +44,10 @@ actor LocationProvider: NSObject, LocationProviderProtocol {
     }
 
     // トラッキング開始
-    func startTracking() {
+    func startTracking(backgroundUpdatesAllowed: Bool) {
+        if backgroundUpdatesAllowed {
+            manager?.allowsBackgroundLocationUpdates = true
+        }
         manager?.startUpdatingLocation()
         isTracking = true
         
