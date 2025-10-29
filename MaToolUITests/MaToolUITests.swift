c77f25900a136d1c5cf35a8c6554d598592221c0
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-10-29 22:57:16 +0900

    リネーム

--- MaToolUITests/MaToolUITests.swift
+++ MaToolUITests/MaToolUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class FesTracking2UITests: XCTestCase {
+final class MaToolUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
