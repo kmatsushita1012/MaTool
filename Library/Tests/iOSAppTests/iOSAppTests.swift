d008a6893f75ba7ad47be88b3be793a408e3e107
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-11 23:45:21 +0900

    PR-iOSテスト#3

--- Library/Tests/iOSAppTests/iOSAppTests.swift
+++ Library/Tests/iOSAppTests/iOSAppTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import MaTool
+@testable import iOSApp
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
