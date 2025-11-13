2d92b53a0ef224d9a565bc080ebcb6130a149bbf
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-13 15:35:30 +0900

    PR-iOSテスト#17

--- Library/Tests/iOSAppTests/iOSAppTests.swift
+++ Library/Tests/iOSAppTests/iOSAppTests.swift
@@ -1,8 +1,18 @@
 import Testing
 @testable import iOSApp
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    let count = 5
-    #expect(count == 4)
+struct iOSAppTests{
+    @Test func test_成功する() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let count = 5
+        #expect(count == 5)
+    }
+
+    @Test func test＿失敗する() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let count = 5
+        #expect(count == 4)
+    }
 }
+
+
