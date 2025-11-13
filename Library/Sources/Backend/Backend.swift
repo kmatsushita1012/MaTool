0ac24138a0c7cc7a1acaf59e6ae772fb34919e37
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-14 00:33:11 +0900

    Backendのエントリを追加

---
+++ Library/Sources/Backend/Backend.swift
@@ -0,0 +1,14 @@
+//
+//  Backend.swift
+//  MaTool
+//
+//  Created by 松下和也 on 2025/11/14.
+//
+
+
+@main
+struct Backend {
+    static func main() async throws {
+        print("Hello from async main")
+    }
+}
