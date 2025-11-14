200b5ec968b06707128b399b4ee116e51917ffa2
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-14 14:02:57 +0900

    PR-BEテスト#1

--- Library/Tests/BackendTests/BackendTests.swift
+++ Library/Tests/BackendTests/BackendTests.swift
@@ -5,3 +5,19 @@
 //  Created by 松下和也 on 2025/10/30.
 //
 
+import Testing
+@testable import iOSApp
+
+struct BackendTests{
+    @Test func test_成功する() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let count = 5
+        #expect(count == 5)
+    }
+
+    @Test func test＿失敗する() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let count = 5
+        #expect(count == 4)
+    }
+}
