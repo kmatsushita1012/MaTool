5f479da7e13c381ee49849052da2f7866ada7293
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-14 14:48:58 +0900

    PR-BEテスト#3

---
+++ Library/Sources/Backend/Backend.swift
@@ -0,0 +1,13 @@
+//
+//  Backend.swift
+//  MaTool
+//
+//  Created by 松下和也 on 2025/11/14.
+//
+
+@main
+struct MaToolAPI {
+    static func main() async throws {
+        print("Hello, World!")
+    }
+}
