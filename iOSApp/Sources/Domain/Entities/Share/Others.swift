57e298dcf69aaa717fce30070539da06b7885192
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-17 21:26:15 +0900

    修正

--- iOSApp/Sources/Domain/Entities/Share/Others.swift
+++ iOSApp/Sources/Domain/Entities/Share/Others.swift
@@ -5,7 +5,9 @@
 //  Created by 松下和也 on 2025/05/24.
 //
 
+import Shared
 import MapKit
+
 public extension Coordinate {
     func toCL() -> CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
