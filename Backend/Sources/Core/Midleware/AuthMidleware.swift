3655ac3a2a651a9d30669188d8d3c5b1eb09d533
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-22 20:19:46 +0900

    Middleware修正

--- Backend/Sources/Core/Midleware/AuthMidleware.swift
+++ Backend/Sources/Core/Midleware/AuthMidleware.swift
@@ -5,30 +5,23 @@
 //  Created by 松下和也 on 2025/10/27.
 //
 
-enum UserRoleType: String, Sendable {
-    case guest, region, district
-}
 
-struct UserRole: Sendable {
-    let type: UserRoleType
-    let id: String?
-}
 
 struct AuthMiddleware: MiddlewareComponent {
     var path: String
     
     var body: Middleware = { req, next in
         var request = req
-        if let auth = req.headers["authorization"] {
-            // ここで Cognito検証などを行う
-            if auth.contains("region") {
-                request.user = UserRole(type: .region, id: "user123")
-            } else {
-                request.user = UserRole(type: .guest, id: nil)
-            }
-        } else {
-            request.user = UserRole(type: .guest, id: nil)
-        }
+//        if let auth = req.headers["authorization"] {
+//            // ここで Cognito検証などを行う
+//            if auth.contains("region") {
+//                request.user = UserRole(type: .region, id: "user123")
+//            } else {
+//                request.user = UserRole(type: .guest, id: nil)
+//            }
+//        } else {
+//            request.user = UserRole(type: .guest, id: nil)
+//        }
         print("Authenticated as \(request.user?.type.rawValue ?? "unknown")")
         return await next(request)
     }
