fe4ae104040151de181964e15e53c03aff3bd887
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-22 23:09:21 +0900

    AuthManagerにaccessTokenを追加

--- Backend/Sources/Data/Auth/CognitoAuthManager.swift
+++ Backend/Sources/Data/Auth/CognitoAuthManager.swift
@@ -50,6 +50,16 @@ struct CognitoAuthManager: AuthManager {
         let user: UserRole = .district(id)
         return user
     }
+    
+    func get(accessToken: String) async throws -> UserRole {
+        let input = GetUserInput(
+            accessToken: accessToken
+        )
+        let response = try await client.getUser(input: input)
+        
+        let user = try parseUserRole(attributes: response.userAttributes, id: response.username)
+        return user
+    }
 
     func get(username: String) async throws -> UserRole {
         let input = AdminGetUserInput(
@@ -58,12 +68,21 @@ struct CognitoAuthManager: AuthManager {
         )
         let response = try await client.adminGetUser(input: input)
         
-        let id = response.username
-        let attributes = response.userAttributes
-        let role = attributes?.first{ $0.name == "custom:role"}
-        guard let id, role?.value == "district" else { throw APIError.unauthorized() }
-        let user: UserRole = .district(id)
+        let user = try parseUserRole(attributes: response.userAttributes, id: response.username)
         return user
+    }
+    
+    private func parseUserRole(attributes: [CognitoIdentityProviderClientTypes.AttributeType]?, id: String?) throws -> UserRole {
+        let role = attributes?.first{ $0.name == "custom:role"}?.value
+        guard let id, let role else { throw APIError.unauthorized() }
+        switch role {
+        case "district":
+            return .district(id)
+        case "region":
+            return .headquarter(id)
+        default:
+            throw APIError.unauthorized()
+        }
     }
 }
 
