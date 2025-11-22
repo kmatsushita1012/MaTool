9f414c6875fd94b248d635ca51050fb09c4895bf
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-22 22:34:48 +0900

    Test追加

---
+++ Backend/Tests/Data/Auth/CognitoAuthManagerTest.swift
@@ -0,0 +1,48 @@
+//
+//  CognitoAuthManagerTest.swift
+//  matool-backend
+//
+//  Created by 松下和也 on 2025/11/22.
+//
+
+import Testing
+import Foundation
+import CasePaths
+import Dependencies
+@preconcurrency import AWSCognitoIdentityProvider
+@testable import Backend
+import Shared
+
+struct CognitoAuthManagerTest {
+    let factory: AuthManagerFactory
+    @Dependency(Environment.self) var env
+    
+    init() {
+        @Dependency(Environment.self) var env
+        self.factory = {
+            let client = try await CognitoIdentityProviderClient()
+            return CognitoAuthManager(client: client, userPoolId: env.cognitoPoolId)
+        }
+    }
+    
+    @Test func test_create_正常() async throws {
+        let username = "integration-test-\(UUID().uuidString.prefix(6))"
+        let subject = try await factory()
+        
+        let result: UserRole = try await subject.create(username: username, email: env.cognitoTestEmail)
+        
+        
+        #expect(result.is( \.district) == true)
+        let usernameCreateResult : String? = result[case: \.district]
+        #expect(usernameCreateResult?.lowercased() == username.lowercased())
+        
+        
+        let getResult: UserRole = try await subject.get(username: username)
+        
+        #expect(getResult.is( \.district) == true)
+        let usernameGetResult : String? = getResult[case: \.district]
+        #expect(usernameGetResult?.lowercased() == username.lowercased())
+    }
+}
+
+ 
