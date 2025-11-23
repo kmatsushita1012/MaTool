e08b5c81130b2ff5c45679446c72a36004eade7d
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-24 00:32:13 +0900

    Package.swift更新

--- Backend/Package.swift
+++ Backend/Package.swift
@@ -16,7 +16,9 @@ let package = Package(
         .package(url: "https://github.com/awslabs/aws-sdk-swift.git", from: "1.5.18"),
         .package(url: "https://github.com/awslabs/swift-aws-lambda-runtime.git", from: "2.0.0"),
         .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "1.2.3"),
-        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.9.0")
+        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.9.0"),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths.git", from: "1.7.2"),
+        .package(url: "https://github.com/thebarndog/swift-dotenv.git", from: "2.1.0")
     ],
     targets: [
         .executableTarget(
@@ -24,13 +26,18 @@ let package = Package(
             dependencies: [
                 "Shared",
                 .product(name: "AWSDynamoDB", package: "aws-sdk-swift"),
+                .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-swift"),
                 .product(name: "Dependencies", package: "swift-dependencies"),
                 .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
                 .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-events"),
-                .product(name: "ServiceLifecycle", package: "swift-service-lifecycle")
+                .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
+                .product(name: "CasePaths", package: "swift-case-paths"),
+                .product(name: "SwiftDotenv", package: "swift-dotenv")
             ],
             path: "Sources",
-            resources: []
+            resources: [
+                .copy("../.env")
+            ]
         ),
         .testTarget(
             name: "BackendTests",
