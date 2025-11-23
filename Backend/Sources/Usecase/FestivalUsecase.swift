ae1d5dad8fa1949fc955a73ba733fd01c8681168
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-23 20:39:19 +0900

    Usecase作成

---
+++ Backend/Sources/Usecase/FestivalUsecase.swift
@@ -0,0 +1,43 @@
+//
+//  FestivalUsecase.swift
+//  matool-backend
+//
+//  Created by 松下和也 on 2025/11/23.
+//
+
+import Dependencies
+import Shared
+
+// MARK: - FestivalUsecaseProtocol
+protocol FestivalUsecaseProtocol: Usecase {
+    func scan() async throws -> [Festival]
+    func get(_ id: String) async throws -> Festival
+    func post(_ festival: Festival, user: UserRole) async throws -> Festival
+}
+
+// MARK: - FestivalUsecase
+struct FestivalUsecase: FestivalUsecaseProtocol {
+    @Dependency(FestivalRepositoryKey.self) var repository
+    
+    func scan() async throws -> [Festival] {
+        try await repository.scan()
+    }
+    
+    func get(_ id: String) async throws -> Festival {
+        guard let result = try await repository.get(id: id) else {
+            throw APIError.notFound("存在しない項目です。")
+        }
+        return result
+    }
+    
+    func post(_ item: Festival, user: UserRole) async throws -> Festival {
+        guard case let .headquarter(headquarterId) = user,
+              headquarterId == item.id else {
+            throw APIError.unauthorized("アクセス権限がありません。")
+        }
+        let result = try await repository.put(item)
+        return result
+    }
+    
+    
+}
