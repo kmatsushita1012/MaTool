bb17f111b1f836a697a113977508f8077e07a98d
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-24 22:19:05 +0900

    Shared修正

--- Shared/Sources/DTO/DistrictDTO.swift
+++ Shared/Sources/DTO/DistrictDTO.swift
@@ -9,9 +9,29 @@
 public struct DistrictTool: DTO {
     public let districtId: String
     public let districtName: String
-    public let regionId: String
-    public let regionName: String
+    public let festivalId: String
+    public let festivalName: String
     public let milestones: [Information]
     public let base: Coordinate
     public let spans: [Span]
+    
+    public init(districtId: String, districtName: String, festivalId: String, festivalName: String, milestones: [Information], base: Coordinate, spans: [Span]) {
+        self.districtId = districtId
+        self.districtName = districtName
+        self.festivalId = festivalId
+        self.festivalName = festivalName
+        self.milestones = milestones
+        self.base = base
+        self.spans = spans
+    }
+}
+
+public struct DistrictCreateDTO: DTO {
+    public let name: String
+    public let email: String
+    
+    public init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
 }
