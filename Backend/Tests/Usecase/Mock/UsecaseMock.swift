57ca0654b3ba6bcfbf4b5ddc0c9796dbc9e11b73
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-24 18:19:12 +0900

    Usecase修正

--- Backend/Tests/Usecase/Mock/UsecaseMock.swift
+++ Backend/Tests/Usecase/Mock/UsecaseMock.swift
@@ -10,16 +10,26 @@ import Testing
 import Shared
 
 final class FestivalUsecaseMock: FestivalUsecaseProtocol, @unchecked Sendable {
-    var scanCallCount = 0
-    var scanHandler: (() throws -> [Festival])? = nil
+    
+    init(scanCallCount: Int = 0, scanHandler: (() throws -> [Festival])? = nil, getCallCount: Int = 0, getHandler: ((String) throws -> Shared.Festival)? = nil, putCallCount: Int = 0, putHandler: ((Shared.Festival, Shared.UserRole) throws -> Shared.Festival)? = nil) {
+        self.scanCallCount = scanCallCount
+        self.scanHandler = scanHandler
+        self.getCallCount = getCallCount
+        self.getHandler = getHandler
+        self.putCallCount = putCallCount
+        self.putHandler = putHandler
+    }
+    
+    private(set) var scanCallCount = 0
+    private var scanHandler: (() throws -> [Festival])? = nil
     func scan() async throws -> [Festival] {
         scanCallCount+=1
         guard let scanHandler else { fatalError("Unimplemented") }
         return try scanHandler()
     }
     
-    var getCallCount = 0
-    var getHandler: ((String) throws -> Shared.Festival)? = nil
+    private(set) var getCallCount = 0
+    private var getHandler: ((String) throws -> Shared.Festival)? = nil
     func get(_ id: String) async throws -> Shared.Festival {
         getCallCount+=1
         guard let getHandler else { fatalError("Unimplemented")}
@@ -27,8 +37,8 @@ final class FestivalUsecaseMock: FestivalUsecaseProtocol, @unchecked Sendable {
         
     }
     
-    var putCallCount = 0
-    var putHandler: ((Shared.Festival, Shared.UserRole) throws -> Shared.Festival)? = nil
+    private(set) var putCallCount = 0
+    private var putHandler: ((Shared.Festival, Shared.UserRole) throws -> Shared.Festival)? = nil
     func put(_ festival: Shared.Festival, user: Shared.UserRole) async throws -> Shared.Festival {
         putCallCount+=1
         guard let putHandler else { fatalError("Unimplemented")}
