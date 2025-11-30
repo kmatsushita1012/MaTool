2e588da9aa1871e67f85b74cbec194ee2f1b7791
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-11-30 16:27:40 +0900

    PUT/POST/DELETEの戻り値変更

--- iOSApp/Sources/Data/ConstantValues.swift
+++ iOSApp/Sources/Data/ConstantValues.swift
@@ -26,7 +26,7 @@ struct ConstantValues:Sendable {
 
 extension ConstantValues: DependencyKey {
     static let liveValue = Self(
-        apiBaseUrl: "https://eqp8rvam4h.execute-api.ap-northeast-1.amazonaws.com",
+        apiBaseUrl: "-district-v2",
         appStatusUrl: "https://studiomk-app-assets.s3.ap-northeast-1.amazonaws.com/MaTool/app-config.json",
         defaultFestivalKey: "region",
         defaultDistrictKey: "district",
