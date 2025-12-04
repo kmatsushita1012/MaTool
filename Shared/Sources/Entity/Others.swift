2ddeaabcd4c8a20b009a71f416b9fe891870045b
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-12-04 15:29:08 +0900

    Legacyを追加

--- Shared/Sources/Entity/Others.swift
+++ Shared/Sources/Entity/Others.swift
@@ -10,31 +10,6 @@ public struct InfoItem: Entity {
     public let description: String?
 }
 
-
-struct Segment: Codable{
-    let id: String
-    let start: Coordinate
-    let end: Coordinate
-    var coordinates: [Coordinate]
-    let isPassed: Bool
-    init(id: String,start: Coordinate, end: Coordinate, coordinates: [Coordinate]? = nil, isPassed: Bool = false) {
-        self.id = id
-        self.start = start
-        self.end = end
-        self.coordinates = coordinates ?? [start, end]
-        self.isPassed = isPassed
-    }
-}
-
-extension Segment: Equatable {
-}
-
-extension Segment: Hashable {
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-}
-
 public struct Pair<Element: Equatable>: Equatable {
     public let first: Element
     public let second: Element
