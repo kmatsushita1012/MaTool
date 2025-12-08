eacb52ac39a460a3c2842ece2f2c1035c77d9c57
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-12-09 01:15:40 +0900

    PeriodEditView

---
+++ iOSApp/Sources/Presentation/StoreView/Admin/Region/Program/PeriodEditView.swift
@@ -0,0 +1,71 @@
+//
+//  PeriodEditView.swift
+//  MaTool
+//
+//  Created by 松下和也 on 2025/12/07.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import NavigationSwipeControl
+
+@available(iOS 17.0, *)
+struct PeriodEditView: View {
+    @SwiftUI.Bindable var store: StoreOf<PeriodEditFeature>
+    @Dependency(\.values.isLiquidGlassEnabled) var isLiquidGlassEnabled: Bool
+    
+    var body: some View {
+        content
+            .dismissible(backButton: false, edgeSwipe: true)
+            .toolbar{
+                if #available(iOS 26.0, *), isLiquidGlassEnabled {
+                    toolbarAfterLiquidGlass
+                } else {
+                    toolbarBeforeLiquidGlass
+                }
+            }
+    }
+    
+    @ViewBuilder
+    var content: some View {
+        Form {
+            Section(header: Text("日付")){
+                DatePicker(selection: $store.period.date)
+            }
+            
+            Section(header: Text("タイトル"), footer: Text("例: 午前、午後、夜")){
+                TextField("タイトルを入力", text: $store.period.title)
+            }
+            
+            Section(header: Text("時刻（目安）")){
+                TimePicker("開始時刻", selection: $store.period.start)
+                TimePicker("終了時刻", selection: $store.period.end)
+            }
+            
+            Section{
+                Button("削除", systemImage: "trash", role: .destructive) {
+                    store.send(.deleteTapped)
+                }
+                .foregroundStyle(.red)
+            }
+        }
+    }
+    
+    @ToolbarContentBuilder
+    var toolbarBeforeLiquidGlass: some ToolbarContent {
+        ToolbarItem(placement: .primaryAction) {
+            Button(systemImage: "checkmark") {
+                store.send(.doneTapped)
+            }
+        }
+    }
+    
+    @ToolbarContentBuilder
+    var toolbarAfterLiquidGlass: some ToolbarContent {
+        ToolbarItem( placement: .primaryAction) {
+            Button(systemImage: "checkmark") {
+                store.send(.doneTapped)
+            }
+        }
+    }
+}
