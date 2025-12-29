743d0282bf6a7750eb3b6b8dbd1c59351631d5c6
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-12-29 15:51:12 +0900

    Router修正

--- Backend/Sources/Router/RouteRouter.swift
+++ Backend/Sources/Router/RouteRouter.swift
@@ -12,9 +12,11 @@ struct RouteRouter: Router {
     @Dependency(RouteControllerKey.self) var routeController
     
     func body(_ app: Application) {
+        app.put(path: "/routes/ids", routeController.getIds)
         app.get(path: "/routes/:routeId", routeController.get)
         app.put(path: "/routes/:routeId", routeController.put)
         app.delete(path: "/routes/:routeId", routeController.delete)
-        app.get(path: "/routes", routeController.getIds)
+        app.get(path: "/routes", routeController.query)
+        app.put(path: "/routes", routeController.post)
     }
 }
