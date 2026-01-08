23e9cda6b07c517c8a3bb2e672d34d09e676f3f8
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-01-08 23:51:45 +0900

    RepositoryをRecordに適合

--- Backend/Sources/Data/Database/Record.swift
+++ Backend/Sources/Data/Database/Record.swift
@@ -21,4 +21,17 @@ struct Record<Content: Entity>: RecordProtocol {
     let pk:  String
     let sk: String
     let content: Content
+    
+    init(pk: String, sk: String, content: Content) {
+        self.pk = pk
+        self.sk = sk
+        self.content = content
+    }
+}
+
+// TODO: マイグレーション後に削除
+extension Record where Content: Identifiable {
+    init(_ content: Content){
+        self.init(pk: content.id as! String, sk: "", content: content)
+    }
 }
