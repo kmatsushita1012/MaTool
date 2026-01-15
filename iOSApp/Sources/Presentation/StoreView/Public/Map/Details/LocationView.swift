cb20f5d5ce5e32cc0465c149edb422fb8b0ab2f0
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-01-15 16:24:39 +0900

    PublicRouteを修正

--- iOSApp/Sources/Presentation/StoreView/Public/Map/Details/LocationView.swift
+++ iOSApp/Sources/Presentation/StoreView/Public/Map/Details/LocationView.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 import Shared
+import SQLiteData
 
 struct LocationView: View {
-    let item: FloatLocationGetDTO
+    let location: FloatLocation
+    @FetchOne var district: District?
+    
+    init(_ location: FloatLocation) {
+        self.location = location
+        self._district = FetchOne(District.where{ $0.id == location.districtId })
+    }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16){
-            BulletItem(text: "\(item.districtName) 屋台")
-            BulletItem(text: item.timestamp.text())
+            BulletItem(text: "\(district?.name ?? "") 屋台")
+            BulletItem(text: location.timestamp.text())
             Spacer()
         }
         .frame(maxWidth: .infinity, alignment: .leading)
