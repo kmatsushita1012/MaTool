9d1c271f1759a7110fe9a4fa18f10d3c713d2331
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-01-17 21:49:36 +0900

    URL変更

--- iOSApp/Sources/Data/ConstantValues.swift
+++ iOSApp/Sources/Data/ConstantValues.swift
@@ -30,7 +30,7 @@ extension ConstantValues: DependencyKey {
     static let liveValue = Self(
         apiBaseUrl: {
             #if DEBUG
-                "-normalization"
+                ""
             #else
                 ""
             #endif
