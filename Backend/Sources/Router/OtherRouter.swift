de4f35d10c1a3fe70c38729fb97fee76590e8a2c
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-01-17 22:06:34 +0900

    PeriodのPutを修正

--- Backend/Sources/Router/OtherRouter.swift
+++ Backend/Sources/Router/OtherRouter.swift
@@ -1,5 +1,5 @@
 //
-//  RouteRouter.swift
+//  OtherRouter.swift
 //  matool-backend
 //
 //  Created by 松下和也 on 2025/11/30.
@@ -7,13 +7,15 @@
 
 import Dependencies
 
-struct RouteRouter: Router {
+struct OtherRouter: Router {
     
     @Dependency(RouteControllerKey.self) var routeController
+    @Dependency(PeriodControllerKey.self) var periodController
     
     func body(_ app: Application) {
         app.get(path: "/routes/:routeId", routeController.get)
         app.put(path: "/routes/:routeId", routeController.put)
         app.delete(path: "/routes/:routeId", routeController.delete)
+        app.put(path: "/periods/:periodId", periodController.put)
     }
 }
