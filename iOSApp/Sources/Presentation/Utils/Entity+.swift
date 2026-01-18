54e4e44fcf2a5718e630721385c64caac8608f82
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-01-18 18:44:41 +0900

    不要なコードを削除

--- iOSApp/Sources/Presentation/Utils/Entity+.swift
+++ iOSApp/Sources/Presentation/Utils/Entity+.swift
@@ -8,26 +8,6 @@
 import MapKit
 import Shared
 
-extension Point{
-    func annotation(type: PointAnnotation.TitleType = .simple) -> PointAnnotation {
-        PointAnnotation(self, type: type)
-    }
-}
-
-extension Array where Element == Point {
-    var pair: [Pair<Point>] {
-        guard count > 1 else { return [] }
-        return zip(self, dropFirst()).map { Pair(first: $0, second: $1) }
-    }
-}
-
-extension Pair where Element == Point {
-    var polyline: PathPolyline {
-        let coords = [self.first, self.second].map { $0.coordinate.toCL() }
-        let polyline = PathPolyline(coordinates: coords, count: coords.count)
-        return polyline
-    }
-}
 
 extension Period {
     var text: String {
