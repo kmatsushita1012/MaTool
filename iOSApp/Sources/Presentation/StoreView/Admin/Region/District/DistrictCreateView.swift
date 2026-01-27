586403b22643074531f501ee73619cf30286525b
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-01-27 19:41:54 +0900

    DistrictCreate

---
+++ iOSApp/Sources/Presentation/StoreView/Admin/Region/District/DistrictCreateView.swift
@@ -0,0 +1,51 @@
+//
+//  DistrictCreateView.swift
+//  MaTool
+//
+//  Created by 松下和也 on 2025/05/12.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import NavigationSwipeControl
+
+@available(iOS 17.0, *)
+struct DistrictCreateView: View {
+    
+    @SwiftUI.Bindable var store: StoreOf<DistrictCreateFeature>
+    
+    var body: some View {
+        List{
+            Section(header: Text("町名")) {
+                TextField("町名を入力",text: $store.name)
+            }
+            Section(header: Text("メールアドレス")) {
+                TextField("メールアドレスを入力",text: $store.email)
+                    .textContentType(.emailAddress)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+            }
+        }
+        .navigationTitle("新規作成")
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("キャンセル") {
+                    store.send(.cancelTapped)
+                }
+                .padding(8)
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button{
+                    store.send(.createTapped)
+                } label: {
+                    Text("作成")
+                        .bold()
+                }
+                .padding(8)
+            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .dismissible(backButton: false)
+        .loadingOverlay(store.isLoading)
+    }
+}
