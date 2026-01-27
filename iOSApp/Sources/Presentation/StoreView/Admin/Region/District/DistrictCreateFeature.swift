d51dc5813d269564fd07c7296a60227014c18fbf
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-01-27 19:41:54 +0900

    DistrictCreate

---
+++ iOSApp/Sources/Presentation/StoreView/Admin/Region/District/DistrictCreateFeature.swift
@@ -0,0 +1,69 @@
+//
+//  AdminFestivalDistrictCreate.swift
+//  MaTool
+//
+//  Created by 松下和也 on 2025/05/12.
+//
+
+import ComposableArchitecture
+import Shared
+
+@Reducer
+struct DistrictCreateFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        let festivalId: Festival.ID
+        var name: String = ""
+        var email: String = ""
+        var isLoading: Bool = false
+        @Presents var alert: Alert.State?
+    }
+    @CasePathable
+    enum Action: Equatable, BindableAction {
+        case binding(BindingAction<State>)
+        case createTapped
+        case cancelTapped
+        case errorCaught(APIError)
+        case alert(PresentationAction<Alert.Action>)
+    }
+    
+    @Dependency(DistrictDataFetcherKey.self) var dataFetcher
+    @Dependency(\.dismiss) var dismiss
+    
+    var body: some ReducerOf<DistrictCreateFeature> {
+        BindingReducer()
+        Reduce{ state, action in
+            switch action {
+            case .binding:
+                return .none
+            case .createTapped:
+                if state.name.isEmpty || state.email.isEmpty {
+                    return .none
+                }
+                state.isLoading = true
+                return .run { [state] send in
+                    let result = await task{ try await dataFetcher.create(name: state.name, email: state.email, festivalId: state.festivalId) }
+                    switch result {
+                    case .success:
+                        await dismiss()
+                    case .failure(let error):
+                        await send(.errorCaught(error))
+                    }
+                }
+            case .cancelTapped:
+                return .run { _ in
+                    await dismiss()
+                }
+            case .errorCaught(let error):
+                state.isLoading = false
+                state.alert = Alert.error(error.localizedDescription)
+                return .none
+            case .alert:
+                state.alert = nil
+                return .none
+            }
+        }
+        .ifLet(\.$alert, action: \.alert)
+    }
+}
