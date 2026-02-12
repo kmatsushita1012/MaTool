00c32cb2f70fa774221deb4f74af809399bbacce
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-12 21:27:50 +0900

    PointEditにisBoundaryの編集フィールド追加

--- iOSApp/Sources/Presentation/StoreView/Admin/District/Route/Point/PointEditFeature.swift
+++ iOSApp/Sources/Presentation/StoreView/Admin/District/Route/Point/PointEditFeature.swift
@@ -10,7 +10,7 @@ import Shared
 import SQLiteData
 
 @Reducer
-struct PointEditFeature{
+struct PointEditFeature {
     enum PointType: Equatable, CaseIterable {
         case checkpoint
         case performance
