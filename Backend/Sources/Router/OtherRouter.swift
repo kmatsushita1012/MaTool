6e3e80ff6b055577df71d52dc86246b345ad7b2b
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-15 13:11:47 +0900

    Router整理

--- Backend/Sources/Router/OtherRouter.swift
+++ Backend/Sources/Router/OtherRouter.swift
@@ -13,10 +13,13 @@ struct OtherRouter: Router {
     @Dependency(PeriodControllerKey.self) var periodController
     
     func body(_ app: Application) {
+        // MARK: - Route
         app.get(path: "/routes/:routeId", routeController.get)
         app.put(path: "/routes/:routeId", routeController.put)
         app.delete(path: "/routes/:routeId", routeController.delete)
+        // MARK: - Period
         app.put(path: "/periods/:periodId", periodController.put)
         app.delete(path: "/periods/:periodId", periodController.delete)
+        app.get(path: "/periods/:periodId", periodController.get)
     }
 }
