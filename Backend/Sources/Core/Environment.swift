c7a1c6af762d9be4f1d337036acfae404e98ae1b
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-15 22:05:16 +0900

    Dotenvの方法を変更

--- Backend/Sources/Core/Environment.swift
+++ Backend/Sources/Core/Environment.swift
@@ -18,6 +18,23 @@ extension Environment: DependencyKey{
     static var liveValue: Environment {
         let poolId: String
         let email: String
+        
+        #if DEBUG
+        do {
+            // このファイルのパス
+            let fileURL = URL(fileURLWithPath: #filePath)
+            // Sources/Backend/Core/Environment.swift なら
+            let projectRoot = fileURL.deletingLastPathComponent() // Environment.swift
+                                     .deletingLastPathComponent() // Core
+                                     .deletingLastPathComponent() // Sources
+            let envPath = projectRoot.appendingPathComponent(".env").path
+            
+            print("Loading .env from:", envPath)
+            try Dotenv.configure(atPath: envPath)
+        } catch {
+            print("Failed to load .env:", error)
+        }
+        #endif
 
         if let envPoolId = ProcessInfo.processInfo.environment["COGNITO_USER_POOL_ID"],
            let envEmail  = ProcessInfo.processInfo.environment["COGNITO_TEST_EMAIL"] {
@@ -26,20 +43,23 @@ extension Environment: DependencyKey{
             email  = envEmail
         } else {
             // ローカル開発：.env ファイルから読み込む場合
-            guard let url = Bundle.module.url(forResource: ".env", withExtension: nil) else {
-                fatalError(".env ファイルを取得できません")
-            }
-            
-            guard let _ = try? Dotenv.configure(atPath: url.path()) else {
-                fatalError("Dotenv.configure に失敗しました")
-            }
-            guard let filePoolId = Dotenv["COGNITO_USER_POOL_ID"]?.stringValue,
-                  let fileEmail  = Dotenv["COGNITO_TEST_EMAIL"]?.stringValue else {
-                fatalError(".env ファイルから環境変数を取得できません")
-            }
-            print("Get env value from Dotenv")
-            poolId = filePoolId
-            email  = fileEmail
+            print(".env ファイルを取得できません")
+//            guard let url = Bundle.module.url(forResource: ".env", withExtension: nil) else {
+//                fatalError(".env ファイルを取得できません")
+//            }
+//            
+//            guard let _ = try? Dotenv.configure(atPath: url.path()) else {
+//                fatalError("Dotenv.configure に失敗しました")
+//            }
+//            guard let filePoolId = Dotenv["COGNITO_USER_POOL_ID"]?.stringValue,
+//                  let fileEmail  = Dotenv["COGNITO_TEST_EMAIL"]?.stringValue else {
+//                fatalError(".env ファイルから環境変数を取得できません")
+//            }
+//            print("Get env value from Dotenv")
+//            poolId = filePoolId
+//            email  = fileEmail
+            poolId = ""
+            email  = ""
         }
         return Environment(
             cognitoPoolId: poolId,
