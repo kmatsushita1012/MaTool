99db385f8cce3e2f348f32f978c45355fb8e66ef
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-18 12:26:28 +0900

    DataFetcher修正

--- iOSApp/Sources/Data/DataFetcher/RouteDataFetcher.swift
+++ iOSApp/Sources/Data/DataFetcher/RouteDataFetcher.swift
@@ -16,8 +16,8 @@ enum RouteDataFetcherKey: DependencyKey {
 protocol RouteDataFetcherProtocol: DataFetcher {
     func fetchAll(districtID: District.ID, query: Query) async throws
     func fetch(routeID: Route.ID) async throws
-    func update(_ route: Route, points: [Point]) async throws
-    func create(districtID: District.ID, route: Route, points: [Point]) async throws
+    func update(_ route: Route, points: [Point], passages: [RoutePassage]) async throws
+    func create(districtID: District.ID, route: Route, points: [Point], passages: [RoutePassage]) async throws
     func delete(_ routeID: Route.ID) async throws
 }
 
@@ -26,6 +26,7 @@ struct RouteDataFetcher: RouteDataFetcherProtocol {
     @Dependency(HTTPClientKey.self) var client
     @Dependency(RouteStoreKey.self) var routeStore
     @Dependency(PointStoreKey.self) var pointStore
+    @Dependency(PassageStoreKey.self) var passageStore
     @Dependency(\.defaultDatabase) var database
 
     func fetchAll(districtID: District.ID, query: Query) async throws {
@@ -39,16 +40,16 @@ struct RouteDataFetcher: RouteDataFetcherProtocol {
         try await syncPack(pack)
     }
 
-    func update(_ route: Route, points: [Point]) async throws {
+    func update(_ route: Route, points: [Point], passages: [RoutePassage]) async throws {
         let token = try await getToken()
-        let draft: RoutePack = .init(route: route, points: points)
+        let draft: RoutePack = .init(route: route, points: points, passages: passages)
         let pack: RoutePack = try await client.put(path: "/routes/\(route.id)", body: draft, accessToken: token)
         try await syncPack(pack)
     }
 
-    func create(districtID: District.ID, route: Route, points: [Point]) async throws {
+    func create(districtID: District.ID, route: Route, points: [Point], passages: [RoutePassage]) async throws {
         let token = try await getToken()
-        let draft: RoutePack = .init(route: route, points: points)
+        let draft: RoutePack = .init(route: route, points: points, passages: passages)
         let pack: RoutePack = try await client.post(path: "/districts/\(districtID)/routes", body: draft, query: [:], accessToken: token)
         try await syncPack(pack)
     }
@@ -65,11 +66,17 @@ struct RouteDataFetcher: RouteDataFetcherProtocol {
         let id = pack.route.id
         try await database.write { db in
             let oldPoints = try pointStore.fetchAll(where: { $0.routeId == id }, from: db)
+            let oldPassages = try passageStore.fetchAll(where: { $0.routeId == id }, from: db)
             let (insertedPoints, deletedPointIds) = oldPoints.diff(with: pack.points)
+            let (insertedPassages, deletedPassageIds) = oldPassages.diff(with: pack.passages)
+            // delete
             try routeStore.delete(pack.route.id, from: db)
             try pointStore.deleteAll(deletedPointIds, from: db)
+            try passageStore.deleteAll(deletedPassageIds, from: db)
+            // insert
             try routeStore.insert(pack.route, at: db)
             try pointStore.insert(insertedPoints, at: db)
+            try passageStore.insert(insertedPassages, at: db)
         }
     }
 
