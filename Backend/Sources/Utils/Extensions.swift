447f4360d41ba1c8aa5c47515f9569174ee2ce24
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-20 01:56:14 +0900

    PeriodのDB保存で開始時刻を考慮

--- Backend/Sources/Utils/Extensions.swift
+++ Backend/Sources/Utils/Extensions.swift
@@ -30,3 +30,9 @@ extension SimpleDate {
         "\(year)-\(String(format: "%02d", month))-\(String(format: "%02d", day))"
     }
 }
+
+extension SimpleTime {
+    var sortableKey: String {
+        "\(String(format: "%02d", hour))-\(String(format: "%02d", minute))"
+    }
+}
