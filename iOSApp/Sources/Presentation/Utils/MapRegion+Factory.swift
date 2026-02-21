ec2073d6eaa40362b06e5b35616249bbb69fdd22
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-21 22:30:36 +0900

    ios: Presentation/Domainの拡張配置を再整理

--- iOSApp/Sources/Presentation/Utils/MapRegion+Factory.swift
+++ iOSApp/Sources/Presentation/Utils/MapRegion+Factory.swift
@@ -8,7 +8,6 @@
 import Foundation
 import MapKit
 import Shared
-import SwiftUI
 
 func makeRegion(_ coordinates: [Coordinate], ratio: Double = 1.1) -> MKCoordinateRegion {
     let minLat = coordinates.map { $0.latitude }.min() ?? 0
@@ -80,77 +79,5 @@ func makeRegion(locations: [FloatLocation], origin: Coordinate) -> MKCoordinateR
         return makeRegion(locations.map { $0.coordinate })
     } else {
         return makeRegion(origin: origin, spanDelta: spanDelta)
-    }
-}
-
-extension View {
-    func stroke(color: Color, width: CGFloat = 1) -> some View {
-        modifier(StrokeModifier(strokeSize: width, strokeColor: color))
-    }
-}
-
-struct StrokeModifier: ViewModifier {
-    private let id = UUID()
-    var strokeSize: CGFloat = 1
-    var strokeColor: Color = .blue
-
-    func body(content: Content) -> some View {
-        if strokeSize > 0 {
-            appliedStrokeBackground(content: content)
-        } else {
-            content
-        }
-    }
-
-    private func appliedStrokeBackground(content: Content) -> some View {
-        content
-            .padding(strokeSize * 2)
-            .background(
-                Rectangle()
-                    .foregroundColor(strokeColor)
-                    .mask(alignment: .center) {
-                        mask(content: content)
-                    }
-            )
-    }
-
-    func mask(content: Content) -> some View {
-        Canvas { context, size in
-            context.addFilter(.alphaThreshold(min: 0.01))
-            if let resolvedView = context.resolveSymbol(id: id) {
-                context.draw(resolvedView, at: .init(x: size.width / 2, y: size.height / 2))
-            }
-        } symbols: {
-            content
-                .tag(id)
-                .blur(radius: strokeSize)
-        }
-    }
-}
-
-extension Color {
-    static let map = Color(red: 255 / 255, green: 183 / 255, blue: 167 / 255)
-    static let info = Color(red: 255 / 255, green: 140 / 255, blue: 89 / 255)
-    static let onboarding = Color(red: 179 / 255, green: 38 / 255, blue: 30 / 255)
-    static let annotation = Color(red: 255 / 255, green: 108 / 255, blue: 76 / 255)
-}
-
-enum Mode: Equatable {
-    case update
-    case create
-}
-
-extension Mode {
-    var title: String {
-        switch self {
-        case .update: return "更新"
-        case .create: return "新規作成"
-        }
-    }
-}
-
-extension View {
-    func roundedRect(_ radius: CGFloat = 12) -> some View {
-        clipShape(.rect(cornerRadius: radius, style: .continuous))
     }
 }
