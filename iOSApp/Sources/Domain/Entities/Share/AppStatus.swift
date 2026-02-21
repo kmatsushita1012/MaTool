586ee8bd80ed3d052a85edc5d1c19e7e8fabb6db
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-21 22:30:36 +0900

    ios: Presentation/Domainの拡張配置を再整理

--- iOSApp/Sources/Domain/Entities/Share/AppStatus.swift
+++ iOSApp/Sources/Domain/Entities/Share/AppStatus.swift
@@ -42,19 +42,3 @@ extension StatusCheckResult: Identifiable {
         }
     }
 }
-
-
-extension String {
-    func isVersion(greaterThanOrEqualTo other: String) -> Bool {
-        let components1 = self.split(separator: ".").compactMap { Int($0) }
-        let components2 = other.split(separator: ".").compactMap { Int($0) }
-        for i in 0..<max(components1.count, components2.count) {
-            let v1 = i < components1.count ? components1[i] : 0
-            let v2 = i < components2.count ? components2[i] : 0
-            if v1 > v2 { return true }
-            if v1 < v2 { return false }
-        }
-        return true
-    }
-}
-
