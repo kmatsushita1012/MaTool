ca2281e2a136d50793485ed9addc3a46659f11cb
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-02-26 15:29:17 +0900

    RepositoryTestのFlakyを応急処置

---
+++ Backend/Tests/Data/Repository/RepositoryTest.swift
@@ -0,0 +1,10 @@
+//
+//  RepositoryTest.swift
+//  matool-backend
+//
+//  Created by 松下和也 on 2026/02/26.
+//
+import Testing
+import DependenciesTestSupport
+
+@Suite(.dependencies) struct RepositoryTest{}
