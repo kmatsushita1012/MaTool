999e01f0032c913e5482e3847ddc465f45cddb11
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-03-28 01:42:05 +0900

    ios: District詳細から再発行専用画面へ遷移可能にする

---
+++ iOSApp/Sources/Presentation/View/Admin/Region/District/DistrictReissueView.swift
@@ -0,0 +1,42 @@
+//
+//  DistrictReissueView.swift
+//  MaTool
+//
+//  Created by Codex on 2026/03/28.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import NavigationSwipeControl
+
+@available(iOS 17.0, *)
+struct DistrictReissueView: View {
+    @SwiftUI.Bindable var store: StoreOf<DistrictReissueFeature>
+
+    var body: some View {
+        List {
+            Section(header: Text("新しいメールアドレス")) {
+                TextField("メールアドレスを入力", text: $store.email)
+                    .textContentType(.emailAddress)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+            } footer: {
+                Text("再発行すると既存アカウントは無効になり、ここで入力したメールアドレスで再登録が必要になります。")
+            }
+        }
+        .navigationTitle("アカウント再発行")
+        .toolbar {
+            ToolbarCancelButton {
+                store.send(.cancelTapped)
+            }
+            ToolbarSaveButton(title: "再発行") {
+                store.send(.reissueTapped)
+            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .dismissible(backButton: false)
+        .loadingOverlay(store.isLoading)
+        .alert($store.scope(state: \.alert, action: \.alert))
+    }
+}
+
