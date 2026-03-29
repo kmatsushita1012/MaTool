1cbb9656a42bb2fef9c19310807a2255864d4c36
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-03-29 22:18:23 +0900

    ios: FestivalInfo画面とInfoMapViewを実装

--- iOSApp/Sources/Presentation/View/Public/Info/Festival/FestivalInfoFeature.swift
+++ iOSApp/Sources/Presentation/View/Public/Info/Festival/FestivalInfoFeature.swift
@@ -6,24 +6,53 @@
 //
 
 import ComposableArchitecture
+import MapKit
 import Shared
 
 @Reducer
 struct FestivalInfoFeature {
-    
+
     @ObservableState
     struct State: Equatable {
-        let item: Festival
+        let festival: Festival
+        var region: MKCoordinateRegion?
+        var isDismissed: Bool = false
+        var isLoading: Bool = false
     }
     
     @CasePathable
-    enum Action: Equatable {
+    enum Action: Equatable, BindableAction {
+        case binding(BindingAction<State>)
         case dismissTapped
+        case mapTapped
     }
     
+    @Dependency(\.dismiss) var dismiss
+    
     var body: some ReducerOf<FestivalInfoFeature> {
+        BindingReducer()
         Reduce{ state,action in
-            return .none
+            switch action {
+            case .binding:
+                return .none
+            case .dismissTapped:
+                if #available(iOS 17.0, *) {
+                    return .dismiss
+                } else {
+                    state.isDismissed = true
+                    return .none
+                }
+            case .mapTapped:
+                state.isLoading = true
+                return .none
+            }
         }
+    }
+}
+
+extension FestivalInfoFeature.State {
+    init(item: Festival) {
+        self.festival = item
+        self.region = makeRegion(origin: item.base, spanDelta: spanDelta)
     }
 }
